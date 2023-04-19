0f0e8e02bbf308a1e575025c50e65aca9467b0d4
ATADEGNON Yannick Crédo Kafui <yatadegnon2002@gmail.com>
2023-04-19 22:41:38 +0100

    Le projet de base

---
+++ src/main/java/com/IAI_GLSI/API_EGA/ApiEgaApplication.java
@@ -0,0 +1,13 @@
+package com.IAI_GLSI.API_EGA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEgaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEgaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/IAI_GLSI/API_EGA/ApiEgaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IAI_GLSI.API_EGA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEgaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
